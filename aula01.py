efecb9f3db524c65b2b399199d2936cee0a70a20
Leandro Antonio Simplicio <nightmare.maggot@gmail.com>
2026-09-14 22:16:48 -0300

    first commit

---
+++ aula01.py
@@ -0,0 +1,12 @@
+#Crie um programa que leia o nome do usuário e mostre uma mensagem de boas-vindas
+
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}, seu ambiente está funcionand!")
+
+#Soma de dois números
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))    
+
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é: {soma}")
